049cb8ae84356d3b2d374742f5864b2476f93ffd
Martin <myork@indeed.com>
2021-11-18 09:20:55 -0800

    Add a global scope with some predefined members

--- src/Compiler/Compiler.cpp
+++ src/Compiler/Compiler.cpp
@@ -2,10 +2,15 @@
 
 using namespace ThorsAnvil::Anvil::Ice;
 
+Type::~Type()
+{}
+
 Compiler::Compiler(std::string const& fileName)
     : file(fileName)
     , parser(*this, file)
-{}
+{
+    currentScope.push_back(std::cref(static_cast<Scope&>(globalScope)));
+}
 
 Compiler::~Compiler()
 {
--- src/Compiler/Compiler.h
+++ src/Compiler/Compiler.h
@@ -8,82 +8,172 @@
 #include <fstream>
 #include <map>
 #include <variant>
+#include <vector>
 
 namespace ThorsAnvil::Anvil::Ice
 {
 
 // Console::print("Hello World");
 
+class Decl;
+class Container
+{
+    std::map<std::string, Decl>     names;
+    public:
+        template<typename T>
+        void add(std::string const& name, T&& nameSpace);
+};
+
 class Void
 {
 };
 
-class Namespace
+class Namespace: public Container
 {
 };
 
-class TypeObject
+class Type
+{
+    public:
+        virtual ~Type() = 0;
+};
+
+class TypeObject: public Type, public Container
 {
 };
 
-class TypeArray
+class TypeArray: public Type
 {
 };
 
-class TypeMap
+class TypeMap: public Type
 {
 };
 
-class TypeFunc
+class TypeFunc: public Type
 {
 };
 
 class Object
 {
+    Type const&   type;
+    public:
+        Object(Type const& type)
+            : type(type)
+        {}
 };
 
 class Statement
 {
 };
+using UPVoid        = std::unique_ptr<Void>;
+using UPNamespace   = std::unique_ptr<Namespace>;
+using UPTypeObject  = std::unique_ptr<TypeObject>;
+using UPTypeArray   = std::unique_ptr<TypeArray>;
+using UPTypeMap     = std::unique_ptr<TypeMap>;
+using UPTypeFunc    = std::unique_ptr<TypeFunc>;
+using UPObject      = std::unique_ptr<Object>;
+using UPStatement   = std::unique_ptr<Statement>;
 
-using DeclValue = std::variant<Void, Namespace, TypeObject, TypeArray, TypeMap, TypeFunc, Statement>;
 enum class DeclType {Void, Namespace, TypeObject, TypeArray, TypeMap, TypeFunc, Object, Statement};
+using DeclValue = std::variant<UPVoid, UPNamespace, UPTypeObject, UPTypeArray, UPTypeMap, UPTypeFunc, UPObject, UPStatement>;
 
 class Decl
 {
     DeclType    type;
     DeclValue   value;
     public:
+        // Temporary;
+        DeclType const&   getType() const {return type;}
+        DeclValue const&  getValue() const {return value;}
+
         Decl()
             : type(DeclType::Void)
-            , value(Void{})
+            , value(std::make_unique<Void>())
         {}
-        Decl(Namespace&& nameSpace)
+        Decl(UPNamespace&& nameSpace)
             : type(DeclType::Namespace)
             , value(std::move(nameSpace))
         {}
+        Decl(UPTypeObject&& typeObject)
+            : type(DeclType::TypeObject)
+            , value(std::move(typeObject))
+        {}
+        Decl(UPTypeArray&& typeArray)
+            : type(DeclType::TypeArray)
+            , value(std::move(typeArray))
+        {}
+        Decl(UPTypeMap&& typeMap)
+            : type(DeclType::TypeMap)
+            , value(std::move(typeMap))
+        {}
+        Decl(UPTypeFunc&& typeFund)
+            : type(DeclType::TypeFunc)
+            , value(std::move(typeFund))
+        {}
+        Decl(UPObject&& object)
+            : type(DeclType::Object)
+            , value(std::move(object))
+        {}
+        Decl(UPStatement&& statement)
+            : type(DeclType::Statement)
+            , value(std::move(statement))
+        {}
 };
 
-class Scope
+class Scope: public Container
 {
-    std::map<std::string, Decl>     names;
+};
+
+class StandardScope: public Scope
+{
     public:
-        void add(std::string const& name, Namespace&& nameSpace)
+        StandardScope()
         {
-            names[name] = std::move(nameSpace);
+            UPTypeFunc    consolePrintMethodType(new TypeFunc);
+            UPObject      consolePrintMethodFunc(new Object(*consolePrintMethodType));
+
+            UPTypeObject  consoleType(new TypeObject);
+            consoleType->add("Print", std::move(consolePrintMethodType));
+            consoleType->add("print", std::move(consolePrintMethodFunc));
+
+            UPObject      consoleObject(new Object(*consoleType));
+
+            UPNamespace   system(new Namespace);
+            system->add("Console", std::move(consoleType));
+            system->add("console", std::move(consoleObject));
+
+            add("System", std::move(system));
+
+            UPNamespace   standard(new Namespace);
+            UPTypeObject  typeInt(new TypeObject);
+            UPTypeObject  typeString(new TypeObject);
+
+            standard->add("Int", std::move(typeInt));
+            standard->add("String", std::move(typeString));
+            add("Std",    std::move(standard));
         }
 };
 
 class Compiler: public Action
 {
-    std::ifstream   file;
-    Parser          parser;
+    std::ifstream           file;
+    Parser                  parser;
+    StandardScope           globalScope;
+    std::vector<std::reference_wrapper<Scope const>>  currentScope;
+
     public:
         Compiler(std::string const& fileName);
         virtual ~Compiler() override;
 
         void go();
 };
+
+template<typename T>
+void Container::add(std::string const& name, T&& nameSpace)
+{
+    names[name] = std::move(nameSpace);
+}
 
 }
 
