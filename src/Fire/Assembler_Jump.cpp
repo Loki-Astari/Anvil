02752c3c1f5953b5df7e25d34919dd64087bf892
Martin <myork@indeed.com>
2021-12-05 00:00:36 -0800

    Add the standard JUMP commands

--- src/Fire/Assembler_Jump.cpp
+++ src/Fire/Assembler_Jump.cpp
@@ -23,38 +23,14 @@ std::uint32_t Assembler::getAddress(std::string const& destination, bool buildSy
 
 bool Assembler::getRegister(std::string const& addressRegValue)
 {
-    if (addressRegValue == "Global")
-    {
-        instructions[0] |= Jump_Reg_Global;
-    }
-    else if (addressRegValue == "FramePointer")
-    {
-        instructions[0] |= Jump_Reg_FramePointer;
-    }
-    else if (addressRegValue == "This")
-    {
-        instructions[0] |= Jump_Reg_This;
-    }
-    else if (addressRegValue == "Extra")
-    {
-        instructions[0] |= Jump_Reg_Extra;
-    }
-    else if (addressRegValue == "StackPointer")
-    {
-        instructions[0] |= Jump_Reg_StackPointer;
-    }
-    else if (addressRegValue == "Expr-1")
-    {
-        instructions[0] |= Jump_Reg_Expr_1;
-    }
-    else if (addressRegValue == "Expr-2")
-    {
-        instructions[0] |= Jump_Reg_Expr_2;
-    }
-    else if (addressRegValue == "Expr-3")
-    {
-        instructions[0] |= Jump_Reg_Expr_3;
-    }
+    if (addressRegValue == "Global")            {instructions[0] |= Jump_Reg_Global;}
+    else if (addressRegValue == "FramePointer") {instructions[0] |= Jump_Reg_FramePointer;}
+    else if (addressRegValue == "This")         {instructions[0] |= Jump_Reg_This;}
+    else if (addressRegValue == "Extra")        {instructions[0] |= Jump_Reg_Extra;}
+    else if (addressRegValue == "StackPointer") {instructions[0] |= Jump_Reg_StackPointer;}
+    else if (addressRegValue == "Expr-1")       {instructions[0] |= Jump_Reg_Expr_1;}
+    else if (addressRegValue == "Expr-2")       {instructions[0] |= Jump_Reg_Expr_2;}
+    else if (addressRegValue == "Expr-3")       {instructions[0] |= Jump_Reg_Expr_3;}
     else
     {
         return false;
@@ -109,34 +85,13 @@ int Assembler::assemble_JumpLength(std::string const& cmd, std::string const& fl
 
 bool Assembler::assemble_JumpConditionFlag(std::string const& flagValue)
 {
-    if (flagValue == "AL")
-    {
-        instructions[0] |= Jump_Condition_AL;
-    }
-    else if (flagValue == "EQ")
-    {
-        instructions[0] |=  Jump_Condition_EQ;
-    }
-    else if (flagValue == "NE")
-    {
-        instructions[0] |=  Jump_Condition_NE;
-    }
-    else if (flagValue == "LT")
-    {
-        instructions[0] |=  Jump_Condition_LT;
-    }
-    else if (flagValue == "GT")
-    {
-        instructions[0] |=  Jump_Condition_GT;
-    }
-    else if (flagValue == "LE")
-    {
-        instructions[0] |=  Jump_Condition_LE;
-    }
-    else if (flagValue == "GE")
-    {
-        instructions[0] |=  Jump_Condition_GE;
-    }
+    if (flagValue == "AL")          {instructions[0] |= Jump_Condition_AL;}
+    else if (flagValue == "EQ")     {instructions[0] |= Jump_Condition_EQ;}
+    else if (flagValue == "NE")     {instructions[0] |= Jump_Condition_NE;}
+    else if (flagValue == "LT")     {instructions[0] |= Jump_Condition_LT;}
+    else if (flagValue == "GT")     {instructions[0] |= Jump_Condition_GT;}
+    else if (flagValue == "LE")     {instructions[0] |= Jump_Condition_LE;}
+    else if (flagValue == "GE")     {instructions[0] |= Jump_Condition_GE;}
     else
     {
         return false;
@@ -159,14 +114,8 @@ int Assembler::assemble_Jump(std::istream& lineStream, bool buildSymbols)
         return 0;
     }
 
-    if (cmd == "Return")
-    {
-        return assemble_JumpReturn(lineStream);
-    }
-    else if (cmd == "Call")
-    {
-        instructions[0] |= Act_Jump | JumpType_Call;
-    }
+    if (cmd == "Return")        {return assemble_JumpReturn(lineStream);}
+    else if (cmd == "Call")     {instructions[0] |= Act_Jump | JumpType_Call;}
     else
     {
         // Unknown command report an error.
